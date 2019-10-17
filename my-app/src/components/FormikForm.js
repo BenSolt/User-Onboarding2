c4e91e057da76e4a0a4176f097cf2c7b6b8ffbc4
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-17 15:21:01 -0400

    adding form info

--- my-app/src/components/FormikForm.js
+++ my-app/src/components/FormikForm.js
@@ -2,3 +2,26 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+
+const Form = ({}) => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        status && setUsers(users => [...users, status])
+    },[status])
+
+reutrn (
+<div className='form'>
+    <Form>
+        <Field type="text" name="name" placeholder="enter name" />
+
+        <Field type="text" name="email" placeholder="enter Email" />
+
+        <Field type="text" name="password" placeholder="enter Password" />
+
+    </Form>
+
+</div>
+
+)
+
+}
