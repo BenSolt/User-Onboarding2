5224ffbde262ac0fae83cd6a8ab6a9c193deef70
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-17 15:32:57 -0400

    form halfway done

--- my-app/src/components/FormikForm.js
+++ my-app/src/components/FormikForm.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const Form = ({}) => {
+const Form = ({values, touched, errors, status}) => {
     const [users, setUsers] = useState([])
     useEffect(() => {
         status && setUsers(users => [...users, status])
@@ -12,16 +12,61 @@ const Form = ({}) => {
 reutrn (
 <div className='form'>
     <Form>
-        <Field type="text" name="name" placeholder="enter name" />
+        <Field 
+        type="text" 
+        name="name" 
+        placeholder="enter name" />
 
-        <Field type="text" name="email" placeholder="enter Email" />
+        <Field 
+        type="text" 
+        name="email" 
+        placeholder="enter Email" />
 
-        <Field type="text" name="password" placeholder="enter Password" />
+        <Field 
+        type="text" 
+        name="password" 
+        placeholder="enter Password" />
 
+        <Field
+            type="checkbox"
+            name="Terms of Service"
+            checked={values.vaccinations}
+          />
+
+        <span className="checkmark" />
+
+
+        <button type="submit">Submit!</button>
+        
     </Form>
 
+{users.map(user = (
+    <ul key={}>
+        <li>Name: {user.name}</li>
+        <li>Email: {user.email}</li>
+        <li>Password: {user.name}</li>
+    </ul>
+))}
 </div>
+  );
+};
+const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Please Enter Name'),
+        email: Yup.string().required('Please Enter Email'),
+        password: Yup.string().required('Please Enter Password'),
+    }),
+    handleSubmit(values, {setStatus}) {
+        axios.post('', values)
+        .then(res => {setStatus(res.data); })
+        .catch(err => console.log(err.response));
+    }
 
-)
-
-}
+})
